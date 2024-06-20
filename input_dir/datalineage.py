5a8e22dfcf04f49254e1101d2085acde3e2946b6
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-20 12:03:25 +0530

    committed  the working spark code

--- input_dir/datalineage.py
+++ input_dir/datalineage.py
@@ -1,17 +1,17 @@
 import io
 import json
 import pandas as pd
+import pyspark
+import boto3
 from pyspark import conf
-from pyspark.shell import sc
+from awsglue.context import GlueContext
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
-import pyspark
-from pyspark.sql import SparkSession
+from pyspark import SparkContext
 
-
-#jar_path = "C:\spark-3.4.3-bin-hadoop3\openlineage-spark_2.12-1.13.1.jar"
-spark = SparkSession.builder.getOrCreate()
+sc =SparkContext.getOrCreate()
 sc.setLogLevel("DEBUG")
+ 
 spark = (SparkSession.builder.master('local').appName('Python Spark SQL basic example')         
          .config('spark.some.config.option', 'some-value')
          .config('spark.openlineage.transport.type', 'http')
@@ -23,34 +23,32 @@ spark = (SparkSession.builder.master('local').appName('Python Spark SQL basic ex
          .config('spark.openlineage.parentJobName', 'airflow_dag.airflow_task')
          .config('spark.openlineage.parentRunId', 'xxxx-xxxx-xxxx-xxxx')
          .getOrCreate())
-sc=spark.sparkContext
-
+ 
+         
 # Your Spark job code
-# input_path = "s3://ddsl-rawdata-bucket/test.csv"
-# output_path = "s3://ddsl-extension-bucket/output.csv"
-# df = spark.read.csv(input_path, header=True)
-# df = spark.read.csv(input_path, header=True)
-# df_transformed = df.withColumn("Column1[0]")
-# df_transformed.write.csv(output_path, mode="overwrite", header=True)
-# first_column = df.columns[0]
-# number = 200
-# df_transformed = df.filter(col(first_column) >= number)
-# df_transformed.write.mode('overwrite').csv(output_path, header=True)
-# df.show()
-# df.write.csv("s3://ddsl-extension-bucket//data_lineage.csv")
-# lineage_data = {
-#     "job_name": "YourSparkJob",
-#     "inputs": [input_path],
-#     "outputs": [output_path],
-# }
-# lineage_json = json.dumps(lineage_data)
+input_path = "s3://ddsl-rawdata-bucket/test.csv"
+output_path = "s3://ddsl-extension-bucket/output.csv"
+df = spark.read.csv(input_path, header=True)
+first_column = df.columns[0]
+number = 200
+df_transformed = df.filter(col(first_column) >= number)
+df_transformed.write.mode('overwrite').csv(output_path, header=True)
+df.show()
+df.write.csv("s3://ddsl-extension-bucket/data_lineage.csv")
+lineage_data = {
+    "job_name": "YourSparkJob",
+    "inputs": [input_path],
+    "outputs": [output_path],
+}
+lineage_json = json.dumps(lineage_data)
+
 # Initialize Glue client
-# glue = boto3.client('glue')
+glue = boto3.client('glue')
 
 # Example S3 location to store lineage information
-# s3_bucket = 's3://ddsl-extension-bucket/'
-# s3_key = 'lineage.json'
+s3_bucket = 's3://ddsl-extension-bucket'
+s3_key = 'lineage.json'
 
 # Upload lineage JSON to S3
-#  # glue.put_data_catalog_lineage_settings(CatalogId='your-catalog-id', LineageSettings={"LineageData": lineage_json})
+df.write.format('json').save('s3://ddsl-extension-bucket/lineage.json')
 spark.stop()
