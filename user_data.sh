433e283a67956a0c2c8151dc502e15251d38d80d
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-21 17:52:13 +0530

    user data script upated

--- user_data.sh
+++ user_data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash   
 sudo su -
-sudo apt update
-sudo apt list --upgradable
-sudo apt install apache2 -y  
-sudo ufw allow 'Apache'
-sudo systemctl start apache2
-sudo systemctl status apache2
-sudo mkdir /var/www/alb
-sudo chown -R $USER:$USER /var/www/alb
-sudo chmod -R 755 /var/www/alb
+apt update
+apt list --upgradable
+apt install apache2 -y  
+ufw allow 'Apache'
+systemctl start apache2
+systemctl status apache2
+mkdir /var/www/alb
+chown -R $USER:$USER /var/www/alb
+chmod -R 755 /var/www/alb
 #sudo echo "<h4>Terraform Learning from $(hostname -f)..</h4>" > /usr/share/nginx/html/index.html
 echo "<h1>Terraform Learning from $(hostname -f)..</h1>" > /var/www/alb/index.html
