29ece978295c0132377eace85db190d58bb4ce15
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-21 16:23:23 +0530

    apache set up

--- user_data.sh
+++ user_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash   
 sudo su - 
-apt update
-apt list --upgradable
-apt install apache2    
-ufw allow 'Apache'
-systemctl status apache2
-mkdir /var/www/alb
-chown -R $USER:$USER /var/www/alb
-chmod -R 755 /var/www/alb
-echo "<h1>Terraform Learning from $(hostname -f)..</h1>" > /var/www/alb/index.html
+    apt update
+    apt list --upgradable
+    apt install apache2    
+    ufw allow 'Apache'
+    systemctl status apache2
+    mkdir /var/www/alb
+    chown -R $USER:$USER /var/www/alb
+    chmod -R 755 /var/www/alb
+    echo "<h1>Terraform Learning from $(hostname -f)..</h1>" > /var/www/alb/index.html
