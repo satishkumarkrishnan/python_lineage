7f47c2f50d6c653bf9c2e6ccd60b76ced67783c7
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-21 17:10:51 +0530

    user data script upated

--- user_data.sh
+++ user_data.sh
@@ -1,13 +1,12 @@
 #!/bin/bash   
+sudo su -
 sudo apt update
-#sudo apt list --upgradable
-#sudo apt install apache2 -y  
-#sudo ufw allow 'Apache'
-#sudo systemctl status apache2
-#sudo mkdir /var/www/alb
-sudo yum update -y
-sudo yum install nginx -y
-sudo service nginx start
-#sudo chown -R $USER:$USER /var/www/alb
-#sudo chmod -R 755 /var/www/alb
-sudo echo "<h4>Terraform Learning from $(hostname -f)..</h4>" > /usr/share/nginx/html/index.html
+sudo apt list --upgradable
+sudo apt install apache2 -y  
+sudo ufw allow 'Apache'
+sudo systemctl status apache2
+sudo mkdir /var/www/alb
+sudo chown -R $USER:$USER /var/www/alb
+sudo chmod -R 755 /var/www/alb
+#sudo echo "<h4>Terraform Learning from $(hostname -f)..</h4>" > /usr/share/nginx/html/index.html
+echo "<h1>Terraform Learning from $(hostname -f)..</h1>" > /var/www/alb/index.html
