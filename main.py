8b8b174cd9b7a81ffde89269fa1b294869cceb7e
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-21 09:26:04 +0530

    working code with filtering

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from pyspark import conf
 from pyspark.shell import sc
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+import boto3
+
+#from boto3_fixtures.contrib import boto3
+from urllib3 import packages
 
 jar_path = "C:\spark-3.4.3-bin-hadoop3\openlineage-spark_2.12-1.13.1.jar"
 spark = SparkSession.builder.getOrCreate()
@@ -26,16 +30,14 @@ spark = (SparkSession.builder.master('local').appName('Python Spark SQL basic ex
 # Your Spark job code
 input_path = "C:\\spark-3.4.3-bin-hadoop3\\test.csv"
 output_path = "C:\\spark-3.4.3-bin-hadoop3\\output.csv"
-#df = spark.read.csv(input_path, header=True)
 df = spark.read.csv(input_path, header=True)
-# df_transformed = df.withColumn("Column1[0]")
-# df_transformed.write.csv(output_path, mode="overwrite", header=True)
-first_column = df.columns[0]
-number = 200
-df_transformed = df.filter(col(first_column) >= number)
-df_transformed.write.mode('overwrite').csv(output_path, header=True)
-df.show()
-df.write.csv("C:\\spark-3.4.3-bin-hadoop3\\data_lineage.csv")
+third_column = df.columns[2]
+age = 40
+
+filtered_df = df.filter(df["age"] >= 40)
+filtered_df.show()
+
+filtered_df.write.csv("C:\\spark-3.4.3-bin-hadoop3\\filtered_data.csv", header=True)
 lineage_data = {
     "job_name": "YourSparkJob",
     "inputs": [input_path],
@@ -43,12 +45,12 @@ lineage_data = {
 }
 lineage_json = json.dumps(lineage_data)
 # Initialize Glue client
-# glue = boto3.client('glue')
+glue = boto3.client('glue')
 
 # Example S3 location to store lineage information
-# s3_bucket = 's3://ddsl-extension-bucket/'
-# s3_key = 'lineage.json'
+s3_bucket = 's3://ddsl-extension-bucket/'
+s3_key = 'lineage.json'
 
 # Upload lineage JSON to S3
-# glue.put_data_catalog_lineage_settings(CatalogId='your-catalog-id', LineageSettings={"LineageData": lineage_json})
+#glue.put_data_catalog_lineage_settings(CatalogId='your-catalog-id', LineageSettings={"LineageData": lineage_json})
 spark.stop()
