f6495cc5b646ff54dd099c83e17376f69de6e945
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-23 06:25:43 +0530

    provisioning ASG to test the sample  lineage

--- user_data.sh
+++ user_data.sh
@@ -1,14 +1,15 @@
 #!/bin/bash   
-sudo apt update
-sudo apt list --upgradable
-sudo apt install -y apache2 
-sudo ufw allow 'Apache'
-sudo systemctl start apache2
-sudo systemctl status apache2
-sudo mkdir /var/www/alb
-sudo chown -R $USER:$USER /var/www/alb
-sudo chmod -R 755 /var/www/alb
-sudo echo "<h1>Terraform Learning from $(hostname -f)..</h1>" > /var/www/alb/index.html 
+sudo su -
+apt update
+apt list --upgradable
+apt install -y apache2 
+ufw allow 'Apache'
+systemctl start apache2
+systemctl status apache2
+mkdir /var/www/alb
+chown -R $USER:$USER /var/www/alb
+chmod -R 755 /var/www/alb
+echo "<h1>Terraform Learning from $(hostname -f)..</h1>" > /var/www/alb/index.html 
 #sleep 600
 #yum update -y
 #yum install nginx -y
