36c9221417b7111dc0547e2facdd4684cf940f11
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-07-01 18:48:32 +0530

    updating the lineage.py

--- input_dir/lineage.py
+++ input_dir/lineage.py
@@ -1,8 +1,4 @@
-import io
-import json
-import pandas as pd
-import pyspark
-import boto3
+from pyspark.sql import SparkSession
 from pyspark import conf
 from awsglue.context import GlueContext
 from pyspark.sql import SparkSession
@@ -22,30 +18,91 @@ spark = (SparkSession.builder.master('local').appName('Python Spark SQL basic ex
          .config('spark.openlineage.parentRunId', 'xxxx-xxxx-xxxx-xxxx')
          .getOrCreate())
 
-# Your Spark job code
-input_path = "s3://ddsl-rawdata-bucket/test.csv"
-output_path = "s3://ddsl-extension-bucket/output.csv"
-df = spark.read.csv(input_path, header=True)
-third_column = df.columns[2]
-age = 40
-filtered_df = df.filter(df[third_column] >= age)
-filtered_df.show()
-filtered_df.write.csv("s3://ddsl-extension-bucket/filtered_data.csv", header=True)
+spark.sparkContext.setLogLevel("INFO")
+sc = spark.sparkContext
+sqlContext = SQLContext(sc)
 
-lineage_data = {
+# Your Spark job code
+input_path = "C://Users//satishkr//PycharmProjects//pythonProject3//pythonProject//lineage//test.csv"
+output_path = "C://Users/satishkr//PycharmProjects//pythonProject3//pythonProject//lineage//dlineage.json"
+
+# Read CSV file
+df = spark.read.csv(input_path, header=True)
+
+# Filter DataFrame based on the third column's values being greater than or equal to 40
+third_column = df.columns[2]
+df_transformed = df.filter(df[third_column] >= 40)
+
+# Write transformed DataFrame to a new CSV file
+df_transformed.write.csv(output_path, mode="overwrite", header=True)
+
+# Show original and filtered DataFrames
+df.show()
+filtered_df = df.filter(df[third_column] >= 40)
+filtered_df.show()
+
+# Collect detailed schema information for lineage
+schema_fields = [{"name": field.name, "type": str(field.dataType)} for field in df.schema.fields]
+
+lineage_df = {
     "job_name": "YourSparkJob",
-    "inputs": [input_path],
-    "outputs": [output_path],
+    "inputs": [{
+        "namespace": "file",
+        "name": input_path,
+        "facets": {
+            "dataSource": {
+                "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.13.1/integration/spark",
+                "_schemaURL": "https://openlineage.io/spec/facets/1-0-1/DatasourceDatasetFacet.json#/$defs/DatasourceDatasetFacet",
+                "name": "file",
+                "uri": input_path
+            },
+            "schema": {
+                "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.13.1/integration/spark",
+                "_schemaURL": "https://openlineage.io/spec/facets/1-1-1/SchemaDatasetFacet.json#/$defs/SchemaDatasetFacet",
+                "fields": schema_fields
+            }
+        },
+        "inputFacets": {}
+    }],
+    "outputs": [{
+        "namespace": "file",
+        "name": output_path,
+        "facets": {
+            "dataSource": {
+                "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.13.1/integration/spark",
+                "_schemaURL": "https://openlineage.io/spec/facets/1-0-1/DatasourceDatasetFacet.json#/$defs/DatasourceDatasetFacet",
+                "name": "file",
+                "uri": output_path
+            },
+            "schema": {
+                "_producer": "https://github.com/OpenLineage/OpenLineage/tree/1.13.1/integration/spark",
+                "_schemaURL": "https://openlineage.io/spec/facets/1-1-1/SchemaDatasetFacet.json#/$defs/SchemaDatasetFacet",
+                "fields": schema_fields
+            }
+        },
+        "outputFacets": {}
+    }]
 }
-lineage_json = json.dumps(lineage_data)
- 
+
+# Convert dictionary to JSON string
+lineage_json = json.dumps(lineage_df, indent=4)
+
+# Write JSON string to a file
+file_path = 'lineage.json'
+with open(file_path, 'w') as file:
+    file.write(lineage_json)
+print(f'Lineage JSON data written to {file_path}')
+
+# Optional: Upload lineage JSON to S3 (commented out)
 # Initialize Glue client
 glue = boto3.client('glue')
- 
+
 # Example S3 location to store lineage information
-s3_bucket = 's3://ddsl-extension-bucket'
+s3_bucket = 's3://ddsl-extension-bucket/'
 s3_key = 'lineage.json'
- 
+
 # Upload lineage JSON to S3
 df.write.format('json').save('s3://ddsl-extension-bucket/lineage.json')
+
+# Stop Spark session
 spark.stop()
