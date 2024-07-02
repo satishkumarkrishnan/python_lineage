8c6b7769933982aeeb3a0b7a30712435ef604996
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-07-02 10:42:12 +0530

    updated user_data script

--- user_data.sh
+++ user_data.sh
@@ -9,9 +9,4 @@ systemctl status apache2
 mkdir /var/www/alb
 chown -R $USER:$USER /var/www/alb
 chmod -R 755 /var/www/alb
-echo "<h1>Terraform Learning from $(hostname -f)..</h1>" > /var/www/alb/index.html 
-#sleep 600
-#yum update -y
-#yum install nginx -y
-#service nginx start
-#echo '<h4> Welcome to AWS</h4>' >> /usr/share/nginx/html/index.html
+echo "<h1>Terraform Learning from $(hostname -f)..</h1>" > /var/www/alb/index.html
